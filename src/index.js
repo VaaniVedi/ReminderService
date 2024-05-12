bd718af9a0a3a9124900154138a60561d03f5ad4
Vani <workwithvanichaturvedi@gmail.com>
2024-05-12 09:53:23 +0530

    Added basic rabbitmq setup

--- src/index.js
+++ src/index.js
@@ -1,30 +1,34 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-//const cron= require("node-cron");
+const cron= require("node-cron");
 const {PORT} = require('./config/serverConfig')
 
-//const {sendBasicEmail} = require('./services/email-service')
+const {createChannel} = require('./utils/messageQueue')
+
+const {sendBasicEmail} = require('./services/email-service')
 const TicketController = require('./controller/ticket-controller')
 
 const jobs = require('./utils/job')
 
-const setupAndStartServer = ()=>{
+const setupAndStartServer =async ()=>{
     const app = express()
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended: true}))
+
+    //const channel = await createChannel()
 
     app.post('/api/v1/tickets',TicketController.create)
 
     app.listen(PORT, ()=>{
         console.log(`Server started at port ${PORT}`);
         jobs();
-        /*
+        
         sendBasicEmail(
             'support@admin.com',
             'workwithvanichaturvedi@gmail.com',
             'This is a testing email',
             'Hey, how are you , I hope you like the support'
-        )*/
+        )
     });
 }
 
